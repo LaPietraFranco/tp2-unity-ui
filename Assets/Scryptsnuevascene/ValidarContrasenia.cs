233f9bf5698fede66a6c77d0b7a1ad801cf9ad5e
LaPietraFranco <101827023+LaPietraFranco@users.noreply.github.com>
2022-04-24 19:33:17 -0300

    fourth and fifth video

---
+++ Assets/Scryptsnuevascene/ValidarContrasenia.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ValidarContrasenia : MonoBehaviour
+{
+    string contraseniacorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+
+    void Start()
+    {
+        contraseniacorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+        public void validarContrasenia()
+        {
+        contraseniaUsuario = ingresoUsuario.text;
+        if(contraseniaUsuario == contraseniacorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
