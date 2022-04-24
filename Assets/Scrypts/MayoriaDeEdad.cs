448c42315d826d6e8f57c77a20b726206b4e5947
LaPietraFranco <101827023+LaPietraFranco@users.noreply.github.com>
2022-04-24 18:33:44 -0300

    Second and third video

--- Assets/Scrypts/MayoriaDeEdad.cs
+++ Assets/Scrypts/MayoriaDeEdad.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MayoriaDeEdad : MonoBehaviour
 {
    public int edadusuaruio;
+    public Text mitexto;
     void Start()
     { 
         if(edadusuaruio >= 18)
         {
+            mitexto.text = "Es mayor de edad";
             Debug.Log("Es mayor de edad");
         }
         else
         {
+            mitexto.text = "No es mayor de edad";
             Debug.Log("No es mayor de edad");
         }
     }
